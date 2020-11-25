c86c046eb85d0ab44bbb6a3cca8b0b93eea3e32c
mazlummerdandemirel <mazlummerdandemirel@gmail.com>
2020-11-25 14:54:13 +0300

    first commit

---
+++ src/test/java/com/assessments/luckyshop/LuckyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessments.luckyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
